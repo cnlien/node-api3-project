1793322d456572169be891f32e23e0d6abccd14d
Chris Lien <clien@sharpencx.com>
2020-09-09 15:31:37 -0400

    logger working. GET requests set up

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const userRouter = require('./users/userRouter.js')
+const helmet = require('helmet');
 
 const server = express();
+
+server.use(express.json());
+server.use(helmet());
+server.use(logger)
+server.use('/api/users', userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-//custom middleware
-
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`Method: ${req.method} request`)
+  console.log(`URL: ${req.url}`)
+  console.log(`AT: ${new Date().toISOString()}`)
+  next();
+}
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
+const Users = require('./userDb.js');
+const Posts = require('./userDb');
+
 const router = express.Router();
 
 router.post('/', (req, res) => {
@@ -11,15 +14,50 @@ router.post('/:id/posts', (req, res) => {
 });
 
 router.get('/', (req, res) => {
-  // do your magic!
+  Users.get(req.query)
+    .then((users) => {
+      res.status(200).json(users)
+    })
+    .catch((err) => {
+      res.status(500).json({
+
+      })
+    })
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  Users.getById(req.params.id)
+    .then((user) => {
+      user 
+        ? res.status(200).json(user)
+        : res.status(404).json({
+          message: "The user with that ID does not exisit"
+        })
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "There was an error retreving the user from the database",
+        err
+      })
+    })
 });
 
 router.get('/:id/posts', (req, res) => {
-  // do your magic!
+  const userId = parseInt(req.params.id)
+  Users.getUserPosts(userId)
+    .then((userPosts) => {
+      userPosts.length != 0
+        ? res.status(200).json(userPosts)
+        : res.status(404).json({
+            messaage: "There are no posts by this user or this user does not exist"
+          })
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "The was an eror retreving the posts from the database",
+        err
+      })
+    })
 });
 
 router.delete('/:id', (req, res) => {
